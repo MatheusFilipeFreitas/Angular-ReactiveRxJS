659e83612900d93ace779b1566c8dbc2dd4227c7
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-09-28 14:40:37 -0300

    Initial Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import {Course, sortCoursesBySeqNo} from '../model/course';
+import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
+import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import { CoursesService } from '../services/courses.service';
+
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  beginnerCourses$: Observable<Course[]>;
+
+  advancedCourses$: Observable<Course[]>;
+
+
+  constructor(private coursesService: CoursesService, private dialog: MatDialog) {
+
+  }
+
+  ngOnInit() {
+    this.coursesService.loadAllCourses();
+  }
+
+  editCourse(course: Course) {
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
+
+    dialogConfig.data = course;
+
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+  }
+
+}
+
+
+
+
